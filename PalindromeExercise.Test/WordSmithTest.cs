189c4ab4155a715e3fe2c7800e0ef7451106a8ea
Jon Frederick <jonFred151@gmail.com>
2023-12-29 18:24:06 -0600

    Completed execrise

---
+++ PalindromeExercise.Test/WordSmithTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise.Test
+{
+    public class WordSmithTest
+    {
+
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Jon", false)]
+        [InlineData("radar", true)]
+        [InlineData("TrueCoder", false)]
+        [InlineData("Madam", true)]
+        [InlineData("Rotor", true)]
+        [InlineData("Level", true)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            var ws = new WordSmith();
+
+            var actual = ws.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ PalindromeExercise/WordSmith.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                return false;
+            }
+
+            string cleanWord = word.Replace(" ", "").ToLower();
+
+            int left = 0;
+            int right = cleanWord.Length - 1;
+
+            while (left < right)
+            {
+                if (cleanWord[left] != cleanWord[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+
+    }
+}
